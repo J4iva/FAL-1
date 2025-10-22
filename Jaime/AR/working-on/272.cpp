d1b11b7a50c19bcfbb6558726772450ff8873c1d
Jaiva <182744934+J4iva@users.noreply.github.com>
2025-10-22 12:46:46 +0200

    Bryan/Jaime Partition

---
+++ Jaime/AR/working-on/272.cpp
@@ -0,0 +1,42 @@
+// Nombre y apellidos: 
+
+#include <iostream>
+
+using namespace std;
+	
+/*
+	Algoritmo para resolver un caso
+		En FAL escribimos una función aparte (debajo de este comentario)
+*/
+
+
+
+
+// Resolución de un caso de prueba, salvo ni no quedan más
+bool casoDePrueba() {
+	// Lectura de los datos de entrada
+		// Si están compuestos por más de un valor, leemos solamente el primero aquí
+    int n;
+    cin >> n;
+	if (!cin)	// no hay ningún dato (final de la entrada)
+		return false;
+	else {
+		// Lectura del resto de los datos de entrada
+			// Si quedó algo por leer, se continúa aquí la lectura
+
+		// Resolución del caso 
+			// Llamamos a la función que aplica el algoritmo 
+
+		// Escritura de la salida
+
+		return true;
+	}
+}
+
+int main() {
+
+	// Resolución de todos los casos
+	while (casoDePrueba());
+
+	return 0;	
+}
