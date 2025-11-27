77dc1bb0775f672e54043d02d06e8c1e9858d7d2
Jaiva <182744934+J4iva@users.noreply.github.com>
2025-11-27 21:24:14 +0100

    220 modified (still working-on state)

--- Jaime/AR/working-on/220.cpp
+++ Jaime/AR/working-on/220.cpp
@@ -4,82 +4,98 @@
 
 using namespace std;
 
-int fila(int d) {
-    return (d - 1) / 3;
-}
+struct FuncArray{
 
-int col(int d) {
-    return (d - 1) % 3;
-}
+    int arr[3][3] ={{1, 2, 3}, {4, 5 , 6}, {7, 8, 9}};
+};
 
-bool gana(int suma, int ultimo, bool usados[]) {
-    int f = fila(ultimo);
-    int c = col(ultimo);
-    bool hayMovimiento = false;
+/*
+	Algoritmo para resolver un caso
+		En FAL escribimos una función aparte (debajo de este comentario)
+*/
+bool posibleGanar(int p, int i, int k);
 
-    for (int d = 1; d <= 9; d++) {
-        if (!usados[d]) {
-            int fd = fila(d);
-            int cd = col(d);
+bool juegoCal(int n, int j, FuncArray const v, int p, int i, int k){
 
-            if (fd == f || cd == c) {
-                if (d == 0) {
-                    continue;
-                }
 
-                hayMovimiento = true;
-                int nuevaSuma = suma + d;
 
-                if (nuevaSuma >= 31) {
-                    continue;
-                } else {
-                    usados[d] = true;
-                    bool ganaRival = gana(nuevaSuma, d, usados);
-                    usados[d] = false;
+    while (i < 3){
+        while (p < 3){
+            if (v.arr[i][p]>0 && n+v.arr[i][p] < 31) return posibleGanar(p, i, k);//posible Ganar
+            else return juegoCal(n, j, v, p+1, i, 31);
+            
+        }
+        p=0;
+        i++;
+    }
+    
 
-                    if (!ganaRival) {
-                        return true;
-                    }
-                }
-            }
+
+    return false;
+   }
+
+
+
+FuncArray modMatriz(FuncArray v, int j){
+    
+    int fila = 0, columna = 0;
+
+    if (j%3==0) columna = 2;
+    else if (j == 8 || 5 || 2) columna = 1;
+    else columna = 0;
+    
+    if (j>6) fila = 2;
+    else if (j>3) fila =1;
+    else fila = 0;
+    
+    for (int i = 0; i < 3; i++){
+        for (int p = 0; p < 3; p++){
+            if (i == columna && p == fila || !(i == columna || p == fila)) v.arr[i][p] = 0;
         }
     }
 
-    if (!hayMovimiento) {
-        return false;
-    }
+    return v;
 
-    return false;
+}
+
+bool posibleGanar(int p, int i, int k){
+    int d;
+    FuncArray newv;
+    int j = newv.arr[i][p];
+    newv = modMatriz(newv, j);
+    return !(juegoCal(k, j, newv, p, i, 31));
+
+
+
 }
 
 
 // Resolución de un caso de prueba
 void casoDePrueba() {
 
-    int n, a;
-    cin >> n >> a;
+	// Lectura de los datos de entrada
+    int n, j;
+    cin >> n >> j;
+	// Resolución del caso 
+    FuncArray v;
+    // Llamamos a la función que aplica el algoritmo 
     
-    bool usados[10];
-    for (int i = 0; i < 10; i++) {
-        usados[i] = false;
-    }
-    usados[a] = true;
-
-    if (gana(n, a, usados)) {
-        cout << "GANA" << '\n';
-    } else {
-        cout << "PIERDE" << '\n';
-    }
+    modMatriz(v, j);
+    
+    if (juegoCal(n, j, v, 0, 0, 31)) cout << "GANA" << '\n';
+    else cout << "PIERDE" << '\n';
+	// Escritura de la salida
 }
 
 int main() {
 
-    int numCasos;
-    cin >> numCasos;
+	// Lectura del número de casos
+	int numCasos;
+	cin >> numCasos;
 	
-    for (int i = 0; i < numCasos; ++i) {
-        casoDePrueba();
-    }
+	// Resolución de todos los casos
+	for (int i = 0; i < numCasos; ++i) 
+		casoDePrueba();
 
-    return 0;
+	return 0;
 }
